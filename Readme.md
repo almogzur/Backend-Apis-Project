45750c1d8a5dca614d10f39bcde4ede42c0f974f
almogzur <almogzur1@gmail.com>
2022-09-06 13:41:37 +0300

    2

---
+++ Readme.md
@@ -0,0 +1,17 @@
+
+for first time deploy 
+-------------
+
+npm run deploy this run npm i all and run react build and start back server 
+
+scripts  options 
+
+-------------- 
+  back
+    Node server.Js
+  Fdev
+    (cd front && npm run start && npm run build )
+  build
+    (cd front && npm run build )
+  Bdev
+     nodemon --inspect ./server.js 80 
---
+++ front/src/App.js
@@ -0,0 +1,18 @@
+import './index.css';
+import React from 'react';
+
+
+class App extends React.Component{
+render(){
+  return( <div id="mdiv">
+  <h1 className="text-center p-5" >Timestamp Microservice</h1>
+  <br/>
+     <a className="text-center p-5 " href="http://localhost:81/api">[Project link] </a>
+     <br/>
+      <a  className="text-center p-3" href="asd"> 123  </a>
+      <br/>
+  </div>
+  )
+} 
+}
+export default App;
