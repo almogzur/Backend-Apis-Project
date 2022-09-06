ab555f43b3ae6b119c51063eb50800def2b072b2
almogzur <almogzur1@gmail.com>
2022-09-06 19:23:32 +0300

    remove const

--- server.js
+++ server.js
@@ -6,7 +6,6 @@ const { dirname } = require('path');
 const pkg = require('./package.json');
 const expressServer = require("./expressServer.js");
 const greenlock = require("greenlock-express") 
-var defaults = await greenlock.defaults();
    greenlock
     .init({
         packageRoot: __dirname,
