6069bdd57a6e2e786250b3bfe80a7dbe6d70d784
almogzur <almogzur1@gmail.com>
2022-09-06 15:01:56 +0300

    greenlock.js add

--- server.js
+++ server.js
@@ -7,46 +7,8 @@ const session = require('express-session');
 const mongoose = require("mongoose");
 const cookieParser= require('cookie-parser');
 const path = require("path");
-// SSL START 
-const pkg = require('./package.json');
-const Greenlock = require('greenlock');
-const greenlock = Greenlock.create({
-    packageRoot: __dirname,
-    configDir: "./greenlock.d/",
-    packageAgent: pkg.name + '/' + pkg.version,
-    maintainerEmail:"almogzur1@gmail.com",
-    staging: true,
-    notify: function(event, details) {
-        if ('error' === event) {
-            // `details` is an error object in this case
-            console.error(details);
-        }
-    }
-});
-greenlock.manager
-    .defaults({
-        agreeToTerms: true,
-        subscriberEmail:"almogzur1@gmail.com"
-    })
-    .then(function(fullConfig) {
-        // ...
-    });
-    const altnames = ['sitecss.online', 'www.sitecss.online'];
-
-    greenlock
-        .add({
-            subject: altnames[0],
-            altnames: altnames
-        })
-        .then(function() {
-            // saved config to db (or file system)
-        });
-
-
-
 require('dotenv').config();
 app.use(bodyParser.urlencoded({extended: false}));
-
 
 app.use(
    session({
@@ -56,6 +18,7 @@ app.use(
    cookie: { secure: false },
    key:'express.sid',
  }));
+
 
 console.log("from console Dir Name  => ",__dirname);
 console.log("from console Dat  => ",Date());
@@ -91,5 +54,4 @@ app.use("/api/:date?",(req, res, next) => {
     }
 });
 
-
-app.listen(PORT, () => { console.log(`server started on port ${PORT}`)});
+module.exports = app
