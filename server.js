4d72c57b1cfb1e84ba1387711f307cf96a0aab2d
almogzur <almogzur1@gmail.com>
2022-09-06 13:41:26 +0300

    1

---
+++ server.js
@@ -0,0 +1,81 @@
+'use strict';
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express(); 
+const PORT = 80;
+const session = require('express-session');
+const mongoose = require("mongoose");
+const cookieParser= require('cookie-parser');
+const path = require("path");
+// SSL START 
+const pkg = require('./package.json');
+const Greenlock = require('greenlock');
+const greenlock = Greenlock.create({
+    packageRoot: __dirname,
+    configDir: "./greenlock.d/",
+    packageAgent: pkg.name + '/' + pkg.version,
+    maintainerEmail: pkg.author,
+    staging: true,
+    notify: function(event, details) {
+        if ('error' === event) {
+            // `details` is an error object in this case
+            console.error(details);
+        }
+    }
+});
+greenlock.manager
+    .defaults({
+        agreeToTerms: true,
+        subscriberEmail: process.env["EMAIL"]
+    })
+    .then(function(fullConfig) {
+        // ...
+    });
+
+require('dotenv').config();
+app.use(bodyParser.urlencoded({extended: false}));
+
+
+app.use(
+   session({
+   secret: process.env["ESSION_SECRET"],
+   resave: true,
+   saveUninitialized: true,
+   cookie: { secure: false },
+   key:'express.sid',
+ }));
+
+console.log("from console Dir Name  => ",__dirname);
+console.log("from console Dat  => ",Date());
+console.log("from console process.env  => ",process.env["ESSION_SECRET"]);
+ 
+app.use(express.static(path.join(__dirname, "front", "build")))
+
+app.get("/",(req, res )=>{  
+  res.sendFile(path.join(__dirname, "front", "build", "index.html"))
+  })
+
+app.use("/api/:date?",(req, res, next) => {
+    const parmsJson = req.params.date;
+    const raw= new Date(parmsJson);
+    const toNum = new Date(Number(parmsJson));
+    console.log("incoming req at /api:date",req.params)
+    console.log(raw)
+
+    if(parmsJson==undefined){
+      res.json({"unix":new Date().getTime(),"utc":new Date()});
+      console.log("undefinde date retunr Date now")
+    }
+
+    else if (raw && toNum == "Invalid Date"  ){
+      res.json({"error":"Invalid Date"});
+      console.log("Date object cant prase return eroor")
+
+    }else{
+      res.json({"unix":parmsJson, "utc":toNum});
+      console.log("passed  object ")
+    }
+});
+
+
+app.listen(PORT, () => { console.log(`server started on port ${PORT}`)});
