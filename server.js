effd63a219a45739cf6f315e76df32d75f336a65
almogzur <almogzur1@gmail.com>
2022-09-06 15:15:23 +0300

    green lock as server first

--- server.js
+++ server.js
@@ -1,57 +1,21 @@
-'use strict';
-const express = require("express");
-const bodyParser = require("body-parser");
-const app = express(); 
-const PORT = 80;
-const session = require('express-session');
-const mongoose = require("mongoose");
-const cookieParser= require('cookie-parser');
-const path = require("path");
-require('dotenv').config();
-app.use(bodyParser.urlencoded({extended: false}));
+/////////////// HTTPS  /////////////
+"use strict";
+const pkg = require('./package.json');
+var exprees = require("./app.js");
 
-app.use(
-   session({
-   secret: process.env["ESSION_SECRET"],
-   resave: true,
-   saveUninitialized: true,
-   cookie: { secure: false },
-   key:'express.sid',
- }));
+require("greenlock-express")
+    .init({
+        // where to find .greenlockrc and set default paths
+        packageRoot: __dirname,
+        // where config and certificate stuff go
+        configDir: "./greenlock.d",
+        // contact for security and critical bug notices
+        maintainerEmail: process.env["EMAIL"],
+        // name & version for ACME client user agent
+        //packageAgent: pkg.name + "/" + pkg.version,
+        // whether or not to run at cloudscale
+        cluster: false
+    })
+    .serve(app);
 
-
-console.log("from console Dir Name  => ",__dirname);
-console.log("from console Dat  => ",Date());
-console.log("from console process.env  => ",process.env["ESSION_SECRET"]);
-console.log("from console process.env  => ",typeof process.env["EMAIL"]);
-
- 
-app.use(express.static(path.join(__dirname, "front", "build")))
-
-app.get("/",(req, res )=>{  
-  res.sendFile(path.join(__dirname, "front", "build", "index.html"))
-  })
-
-app.use("/api/:date?",(req, res, next) => {
-    const parmsJson = req.params.date;
-    const raw= new Date(parmsJson);
-    const toNum = new Date(Number(parmsJson));
-    console.log("incoming req at /api:date",req.params)
-    console.log(raw)
-
-    if(parmsJson==undefined){
-      res.json({"unix":new Date().getTime(),"utc":new Date()});
-      console.log("undefinde date retunr Date now")
-    }
-
-    else if (raw && toNum == "Invalid Date"  ){
-      res.json({"error":"Invalid Date"});
-      console.log("Date object cant prase return eroor")
-
-    }else{
-      res.json({"unix":parmsJson, "utc":toNum});
-      console.log("passed  object ")
-    }
-});
-
-module.exports = app
+/////////////// HTTPS END /////////////
