93d061ae4ec05c901cd385da523515ea0685090c
“Almog <almogzur1@gmail.com>
2022-09-11 16:17:44 +0000

    test

--- expressServer.js
+++ expressServer.js
@@ -41,7 +41,7 @@ app.use("/api/:data?",(req, res, next) => {
     console.log("incoming req at /api:date" , SR  , SRparms )
 
     if( SRparms == undefined){
-      res.json({"unix":new Date().getDate(),"utf":new Date()})
+      res.json({"unix":new Date().getTime(),"utf":new Date()})
     } 
   }
 )
