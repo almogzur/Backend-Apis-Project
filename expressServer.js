f91b718b688b873030d200de5f8f923f86ebcc66
“Almog <almogzur1@gmail.com>
2022-09-12 11:50:26 +0000

    cros

--- expressServer.js
+++ expressServer.js
@@ -5,11 +5,17 @@ const app = express();
 const session = require('express-session');
 const mongoose = require("mongoose");
 const cookieParser= require('cookie-parser');
+const cros = require("cros")
 const path = require("path");
 require('dotenv').config();
 const PORT = 80
 app.use(bodyParser.urlencoded({extended: false}));
 const ER = "Invalid Date"
+app.use(cros())
+
+const crosOptions = {
+  origin:"https://www.freecodecamp.org/"
+}
 
 app.use(
    session({
@@ -30,7 +36,7 @@ app.get("/",(req, res )=>{
   res.sendFile(path.join(__dirname, "front", "build", "index.html"))
   })
 
-app.use("/api/:data?",(req, res, next) => {  
+app.use("/api/:data?",cros(crosOptions),(req, res, next) => {  
     const SR = req.params ; // Server Req 
     const SRparms = req.params.data ;  // SR parms 
     const s = new Date(SRparms);  // string
