b44cf4eace1e028f559c624158b3b505bec6cffb
almogzur <almogzur1@gmail.com>
2022-09-14 00:48:35 +0300

    front links

--- front/src/App.js
+++ front/src/App.js
@@ -7,10 +7,10 @@ render(){
   return( <div id="mdiv">
   <h1 className="text-center p-5" >Timestamp Microservice</h1>
   <br/>
-     <a className="text-center p-2 " href="http://localhost/api/2020-01-01">[Url]/api/2020-01-01</a>
+     <a className="text-center p-2 " href="https://sitecss.online/api/2020-01-01">[Url]/api/2020-01-01</a>
      <br/>
      <br/>
-      <a  className="text-center p-3" href="http://localhost/api/1354891983219"> [Url]/api/1354891983219 </a>
+      <a  className="text-center p-3" href="https://sitecss.online/api/1354891983219"> [Url]/api/1354891983219 </a>
       <br/>
       <h4 className="p-5">{JSON.stringify({"unix":1451001600000,"utc":"Fri, 25 Dec 2015 00:00:00 GMT"})}</h4>
       <h2>Made By AlmogZur</h2>
