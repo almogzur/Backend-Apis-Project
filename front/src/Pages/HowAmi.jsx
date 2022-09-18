5ea429921463c70c990aa3b4998719ea0651c74a
“Almog <almogzur1@gmail.com>
2022-09-18 09:52:56 +0000

    Front

--- front/src/Pages/HowAmi.jsx
+++ front/src/Pages/HowAmi.jsx
@@ -1,6 +1,4 @@
-import React from 'react';
- import "./Howami.css"
- 
+
  export default class HowamI extends React.Component{
     constructor(props) {
       super(props);
@@ -8,7 +6,7 @@ import React from 'react';
     }
     render(){
     return (
-      <div className="App">
+      <div className="howami">
         <h1 >API Project: Request Header Parser Microservice</h1>
         <h4>Example Usage:</h4>
         <a href="sitecss.online/api/whoami">[URL]/api/howami</a>
