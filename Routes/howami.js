be2f856dd14ac877dabb1ec6f4c4669002eb7478
“Almog <almogzur1@gmail.com>
2022-09-18 12:18:17 +0000

    cros

--- Routes/howami.js
+++ Routes/howami.js
@@ -2,15 +2,27 @@
 const express = require("express");
 const app = express(); 
 const path = require("path");
+const cors = require("cors")
+
+const corsOptions = {
+    origin:"https://www.freecodecamp.org",
+    optionsSuccessStatus: 200
+  }
+
 
 module.exports =  function howami (app){ 
 
 
-app.get("/api/howami",(req,res)=>{
-    console.log(req.header('x-forwarded-for'))
+app.get("/api/howami",cors(corsOptions),(req,res)=>{
+ const ipH1 = req.headers['x-real-ip']
+ const ipH2 = req.connection.remoteAddress;
+ console.log()
+
+
     res.json({
-        "ip":"",
-        "Os":""
+        "ipaddress": ipH1? ipH1 : ipH2,
+        "language":req.headers['accept-language'],
+        "software":req.headers['user-agent']
     })
 })
 }
--- front/src/Pages/HowAmi.jsx
+++ front/src/Pages/HowAmi.jsx
@@ -14,7 +14,7 @@ import React from 'react';
         <br/>
         <br/>
         <h4>Example Output:</h4>
-        <p></p>
+        <p>{JSON.stringify({"ip":"xxx.xxx.xxx.xxx","language":"en-US,en;q=0.9,he;q=0.8","software":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36"})}</p>
         <h5>Made by Almogzur</h5>
       </div>
     );
