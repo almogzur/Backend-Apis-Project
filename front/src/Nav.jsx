6e8d9f3cb25ad3a60429983b2e132c1376bb6889
almogzur <almogzur1@gmail.com>
2022-09-18 14:10:14 +0300

    front

--- front/src/Nav.jsx
+++ front/src/Nav.jsx
@@ -3,11 +3,11 @@ import { BrowserRouter, Route, Link } from "react-router-dom";
 
 function Navbar() {
   return (
-    <nav>
-          <Link to="/">Home</Link>
-          <Link to="/TimeService">TimeService</Link>
-          <Link to="/Howami">Howami</Link>
-    </nav>
+    <div id="nav">
+    <Link to="/">Home | </Link>
+    <Link to="/TimeService">TimeService | </Link>
+    <Link to="/Howami">Howami </Link>
+   </div>
   );
 }
 
