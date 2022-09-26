41b82d99ae7db261e6b3a096476060ffb2544b3e
“Almog <almogzur1@gmail.com>
2022-09-26 13:15:00 +0000

    moved to all Route enabled cros in Main express

--- Routes/Url.js
+++ Routes/Url.js
@@ -1,10 +1,5 @@
 
 const path = require("path");
-const cors = require("cors");
-const corsOptions = {
-  origin: "https://www.freecodecamp.org",
-  optionsSuccessStatus: 200
-}
 
 exports.UrlShort = function UrlShort(app,db) {
    let counte = 1
@@ -13,55 +8,57 @@ exports.UrlShort = function UrlShort(app,db) {
     
      const db_URLS = await client.db("Urlshort").collection("urls")
 
-        app.route("/api/urlsort/:url?")
+        app.route("/api/shorturl/:url?")
 
        .post(cors(corsOptions),(req,res,next)=>{ // HTTP POST REQ Hendler to update data 
-
-        console.log(`req to "/api/urlshort/" Hendler POST  `);  ++counte ;
-        const par = req.params
+        console.log(req.params)
+        console.log(`POST "/api/shorturl/" counte `);  ++counte ;
         const  url = req.params.url;
-        console.log(par)
         let reg = /[(http(s)?):\/\/(www\.)?a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g //<=  taken forn https://regexr.com/39nr7
         let result = url.match(reg)
+ 
         
-        result ? 
+        if(result!==null){
+          console.log("no Null")
         db_URLS.findOne({url:result.toString()},function(err,url){
-
           if(err){
           console.log(err,"find");
-          next(err);
           }else if(url){
 
            console.log(url, "from find")
+
+           res.json({"url":url.url})
+
           }else{
           db_URLS.insertOne(
-                  { "url":result.toString(),"num":counte }),
+                  { "url":result.toString(),"num":counte },
            function(err,doc){
              if(err){
               console.log(err)
              }else{
               console.log(doc,"from insert one")
               next(null,doc)
+            
             }
-          }}
-         }): res.json({ "error": 'invalid url' })
-
+          })}
+         })} 
+          else{
+            console.log("err bad URL ")
+            res.json({"error":"invalid url"})
+          }
         })
       
   
-       .get(cors(corsOptions),function(req,res){ // retrive data from db 
-
+       .get(function(req,res){ // retrive data from db 
         const url = req.params.url;
-
-        console.log("GET  //api/urlsort/:url? ")
-
-        console.log(db_URLS)
+        console.log("GET  //api/shorturl/:url? ")
 
         db_URLS.findOne({url:url} , function(err,url){
           if(err)
           console.log(err, "from Get find one")
           else if(url)
           console.log(url ,"from Get find one")
+          res.json({"url":url.url})
         })
       })
   })
