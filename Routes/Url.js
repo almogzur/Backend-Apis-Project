f422e2f4ac52af476585a6822ecb9878ef43aeab
“Almog <almogzur1@gmail.com>
2022-09-27 11:31:51 +0000

    creat index

--- Routes/Url.js
+++ Routes/Url.js
@@ -15,57 +15,41 @@ exports.UrlShort = function UrlShort(app,db) {
         let url = req.body.url
         let reg = /[(http(s)?):\/\/(www\.)?a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g //<=  taken forn https://regexr.com/39nr7
         let result = url.match(reg)
-        
+     
+        db_URLS.createIndex( { num: 1 } )
+
         if(result!==null){
-          console.log("no Null")
-        db_URLS.findOne({url:result.toString()},function(err,url){
+              console.log("no Null")
+              db_URLS.findOne({url:result.toString()},
+               (err,url)=>{
           if(err){
-          console.log(err,"find");
+                 console.log(err,"find");
           }else if(url){
-
-           console.log(url, "from find")
-
-           res.json({"url":url.url})
-
+                       console.log(url, "from find")
+                             res.json({ original_url : url.url, short_url : url._id})
           }else{
-          db_URLS.insertOne(
-                  { "url":result.toString(),"num":counte },
-           function(err,doc){
-             if(err){
-              console.log(err)
-             }else{
-              console.log(doc,"from insert one")
-              next(null,doc)
-            
+                db_URLS.insertOne(
+                                 { "url":result.toString(), _id:"" },
+                    (err,doc)=>{
+                                   if(err){
+                                           console.log(err)
+                                    }else{
+                                         console.log(doc,"from insert one")
+                                         next(null,doc)
             }
           })}
          })} 
           else{
-            console.log("err bad URL ")
-            res.json({"error":"invalid url"})
+              console.log("err bad URL ")
+              res.json({"error":"invalid url"})
           }
            
         })
-     
-  
-       .get(function(req,res){ // retrive data from db 
-        const url = req.params.url;
-        console.log("GET  //api/shorturl/:url? ")
-
-        db_URLS.findOne({url:url} , function(err,url){
-          if(err)
-          console.log(err, "from Get find one")
-          else if(url)
-          console.log(url ,"from Get find one")
-          res.json({"url":url.url})
-        })
-      })
   })
          
 }
     
     
        
-      
-
+  
 console.log(module.exports,"exports from Timeservice Route")
