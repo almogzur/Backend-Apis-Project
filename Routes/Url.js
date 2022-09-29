6daaecf655ad192e853e8f63b9245836edbfd142
“Almog <almogzur1@gmail.com>
2022-09-29 10:39:36 +0000

    woking whid mongo _ids

--- Routes/Url.js
+++ Routes/Url.js
@@ -1,23 +1,21 @@
 
 exports.UrlShort = function UrlShort(app,db) {
 
-   let counte = 1
-
    db(async (callback) => { ///api/urlsort/:url?
     
      const db_URLS = await callback.db("Urlshort").collection("urls")
-         
+
+
         app.route("/api/shorturl/:url?")
 
        .post((req,res,next)=>{ // HTTP POST REQ Hendler to update data 
 
-        console.log(`POST /api/shorturl/`);  ++counte ;
+        console.log(`POST /api/shorturl/`);  ;
 
         let url = req.body.url
         let reg = /[(http(s)?):\/\/(www\.)?a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g //<=  taken forn https://regexr.com/39nr7
         let result = url.match(reg)
         
-
         if(result!==null){
               console.log("no Null")
               db_URLS.findOne({url:result.toString()},
@@ -44,6 +42,22 @@ exports.UrlShort = function UrlShort(app,db) {
               res.json({"error":"invalid url"})
           }
            
+        })
+        .get((req,res,next)=>{
+         const Rurl = req.params.url
+          
+         db_URLS.findOne({_id:Rurl},function(err,url){
+          if(!err){
+            console.log(url)
+          }
+         })
+
+          if(!Rurl){
+            //db_URLS.findOne({})
+            res.json({"blbl":"blbla"})
+         }else{
+          res.json({"ble":"ble"})
+         }
         })
   })
          
