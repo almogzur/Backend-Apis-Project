3b9ef5ca246b2d1ccbb6ba27c908d22dcecfd534
“Almog <almogzur1@gmail.com>
2022-10-07 18:10:17 +0000

    dbschema

--- front/src/Pages/workout.jsx
+++ front/src/Pages/workout.jsx
@@ -1,11 +1,24 @@
-
-
+const { useState } = require("react")
 
 exports.WorkOut = ()=>{
 
+const [state , setState]= useState("")
 
+const click = ()=>{
+    setState("click")
 
-
+}
+return (
+    <div>
+        <h1>{state}</h1>
+        <form action="submit">
+            <input type="text" />
+        </form>
+        <button
+        type="submit"
+        onClick={click}>click me</button>
+    </div>
+)
 
     
 }
