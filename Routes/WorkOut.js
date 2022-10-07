6b3f4287c0be4efda6fe02f04c16acc308435f20
“Almog <almogzur1@gmail.com>
2022-10-07 12:42:16 +0000

    Wor

--- Routes/WorkOut.js
+++ Routes/WorkOut.js
@@ -5,9 +5,18 @@ exports.WorkOur= function(app,db){
     db(async(callback)=>{
 
   const db_users = await callback.db("WorkOut").collection("users")
+  const db_log = await callback.db("WorkOut").collection("logs")
+  const db_exercises= await callback.db("WorkOut").collection("exercises")
 
 
-  app.route('/api/')
+  app.route('/api/users/:_id/')
+  .post((req,res,next)=>{
+
+  })
+
+  .get((req,res,next)=>{
+
+  })
 
     })
 }
--- front/src/Pages/workout.jsx
+++ front/src/Pages/workout.jsx
@@ -1,3 +1,11 @@
+
+
+
 exports.WorkOut = ()=>{
+
+
+
+
+
     
 }
