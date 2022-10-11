4616fbbdd24272d2d6a83a961965b964418f804f
“Almog <almogzur1@gmail.com>
2022-10-11 09:36:21 +0000

    db .S / new route

--- db.schema.js
+++ db.schema.js
@@ -1,3 +1,21 @@
-exports.schema = ()=>{
+
+const mongoose = require('mongoose')
+const { Schema } = mongoose;
+const ObjectId = require('mongodb').ObjectID;
+
+const userSchema = new Schema({
+        "Name":String,
+        "count":Number,
+        "id":ObjectId,
+        "logs":[{
+              "description": String,
+              "duration": Number,
+              "date": String,
+        }]
+        
+    })
+
+
+
     
-}
+exports.userSchema = userSchema
