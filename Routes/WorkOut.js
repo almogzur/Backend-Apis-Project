eb6b58842ce544b8e97310a06493fb5e5f2a56e2
“Almog <almogzur1@gmail.com>
2022-10-14 14:52:21 +0000

    working modle db calls

--- Routes/WorkOut.js
+++ Routes/WorkOut.js
@@ -3,13 +3,18 @@ const findAllUsers = require('../db.schema').findAllUsers
 
   exports.WorkOut= function WorkOut(app){
     app.route('/workout/api/:users?')
+    
       .post((req,res,next)=>{
           console.log("POST /workout/api/:user?/")
-       //   console.log(req.body)
+          console.log(req.body)
           const inputUser = req.body.user;
+          const userName = req.body.username
           if(inputUser){
             findUserOrSave(inputUser)
-          }else{
+          }else if(userName){
+            findUserOrSave(userName)
+          }
+          else{
             console.log("no user")
           }
           
